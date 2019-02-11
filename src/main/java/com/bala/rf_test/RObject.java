7524f64c3c16882b728facb2ab7e200578c5530d
rhdmAdmin <>
2019-02-11 11:38:45 +0800

    {/src/main/java/com/bala/rf_test/RObject.java}

--- src/main/java/com/bala/rf_test/RObject.java
+++ src/main/java/com/bala/rf_test/RObject.java
@@ -11,6 +11,29 @@ public class RObject implements java.io.Serializable {
     public RObject() {
     }
 
+    private java.lang.String field1;
+	private java.lang.Integer field2;
+
+	public java.lang.String getField1() {
+		return this.field1;
+	}
+
+	public void setField1(java.lang.String field1) {
+		this.field1 = field1;
+	}
+
+	public java.lang.Integer getField2() {
+		return this.field2;
+	}
+
+	public void setField2(java.lang.Integer field2) {
+		this.field2 = field2;
+	}
+
+	public RObject(java.lang.String field1, java.lang.Integer field2) {
+		this.field1 = field1;
+		this.field2 = field2;
+	}
 
 
 
